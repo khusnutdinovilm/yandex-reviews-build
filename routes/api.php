0f9310d8e912c6254caca528c4821dfc6fb52122
i.khusnutdinov <i.khusnutdinov.pampadu@gmail.com>
2026-09-15 19:08:28 +0500

    feat(auth): аутентификация через sanctum (cookie-based spa)

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', fn (Request $r) => $r->user());
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
