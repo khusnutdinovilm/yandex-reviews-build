88be4e5620c7ebf3a93cfbb9519c9b744879900f
khusnutdinovilm <khusnutdinov.ilmir.w7@gmail.com>
2026-09-17 17:59:21 +0500

    feat(dashboard): экран организации с отзывами и форма настроек

--- resources/js/pages/dashboard/dashboard-page.vue
+++ resources/js/pages/dashboard/dashboard-page.vue
@@ -1,15 +1,64 @@
 <template>
-  <page-template page-title="Dashboard Page">
-    <p>
-      Информация об организации (оценка, кол-во отзывов и т.п.) + список отзывов с пагинацией (50шт)
-    </p>
+  <page-template
+    page-title="Дашборд"
+    :is-content-loading="isOrganizationLoading"
+    :is-content-empty="isOrganizationEmpty"
+    class="dashboard-page"
+  >
+    <template #content-empty>
+      Нет загруженной организации. Перейдите на
+      <router-link :to="{ name: RouteName.Settings }">страницу настроек</router-link>, чтобы
+      добавить организацию
+    </template>
+    <template #default>
+      <organization-card v-if="organization" :organization="organization" />
+
+      <reviews-table
+        ref="tableRef"
+        :reviews="reviews"
+        :total-records="totalRecords"
+        :first="first"
+        :rows="PER_PAGE"
+        :loading="isReviewsLoading"
+        @page="onReviewsPageChange"
+      />
+    </template>
   </page-template>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from "vue";
+
+import { PER_PAGE, RouteName } from "@/shared/config";
+
+import { OrganizationCard, useOrganization } from "@/entities/organization";
 import { PageTemplate } from "@/shared/ui/page-template";
+import { ReviewsTable, useReviews } from "@/entities/review";
 
 defineOptions({
   name: "dashboard-page",
+});
+
+const {
+  organization,
+  isLoading: isOrganizationLoading,
+  isEmpty: isOrganizationEmpty,
+  loadOrganization,
+} = useOrganization();
+
+const {
+  reviews,
+  initialPage,
+  tableRef,
+  totalRecords,
+  first,
+  isLoading: isReviewsLoading,
+  loadReviews,
+  onReviewsPageChange,
+} = useReviews();
+
+onMounted(async () => {
+  await loadOrganization();
+  await loadReviews(initialPage);
 });
 </script>
--- resources/js/pages/settings/settings-page.vue
+++ resources/js/pages/settings/settings-page.vue
@@ -1,13 +1,36 @@
 <template>
-  <page-template page-title="Settings Page">
-    <p>Страница с формой отправки url организации и с дальнейшим перенаправлением на /</p>
+  <page-template
+    page-title="Настройки"
+    class="settings-page"
+    page-content-class="settings-page-content"
+  >
+    <template #default>
+      <connect-organization-form @success="onSuccess" style="height: 75px" />
+    </template>
   </page-template>
 </template>
 
 <script setup lang="ts">
 import { PageTemplate } from "@/shared/ui/page-template";
+import { ConnectOrganizationForm } from "@/features/connect-organization";
+import { useRouter } from "vue-router";
+import { RouteName } from "@/shared/config";
 
 defineOptions({
   name: "settings-page",
 });
+
+const router = useRouter();
+
+const onSuccess = () => {
+  router.push({ name: RouteName.Dashboard });
+};
 </script>
+
+<style lang="scss">
+.settings-page-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+</style>
